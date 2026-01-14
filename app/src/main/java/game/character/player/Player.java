8222ee167a1fbda4e2b9930de24f36b9d5af2023
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-15 03:32:52 +0900

    feat: implement player class

---
+++ app/src/main/java/game/character/player/Player.java
@@ -0,0 +1,12 @@
+package game.character.player;
+
+import game.character.Character;
+
+public class Player extends Character {
+    
+    public Player(String name, int hp, int power) {
+        super(name, hp, power);
+    }
+
+    
+}
