ea41b6de5082854680dce2e4620c3adf66a019cd
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-15 04:28:35 +0900

    feat: implement gameManager class

--- app/src/main/java/game/Main.java
+++ app/src/main/java/game/Main.java
@@ -1,23 +1,9 @@
 package game;
-import game.character.player.Player;
-import game.character.sushi.CucumberRoll;
-import game.character.sushi.RolledSushi;
-import game.character.sushi.Sushi;
-import game.character.sushi.Tuna;
 
 public class Main {
     public static void main(String[] args) {
-        Sushi tuna = new Tuna(100, 10);
-        tuna.greeting();
+        GameManager gameManager = new GameManager();
 
-        RolledSushi cucumberRoll = new CucumberRoll(50, 50);
-        cucumberRoll.roll();
-
-        Player player = new Player("ムラサキ", 200, 50);
-        player.greeting();
-        player.knifeAttack(tuna);
-        player.teaAttack();
-        player.nattoAttack();
-        player.healed(10);
+        gameManager.gameStart();
     }
 }
