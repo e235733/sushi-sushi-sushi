39f4288a1d0dd100e956e846b1f6440f0b8fc200
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-01-15 21:14:21 +0900

    feat: implement damage system

--- app/src/main/java/game/character/sushi/RolledSushi.java
+++ app/src/main/java/game/character/sushi/RolledSushi.java
@@ -1,12 +1,16 @@
 package game.character.sushi;
 
+import game.character.player.Player;
+
 public abstract class RolledSushi extends Sushi {
     
     public RolledSushi(String name, int hp, int power) {
         super(name, hp, power);
     }
 
-    public void roll() {
+    // 巻き寿司の攻撃 roll は、体当たりで大ダメージを与えるが自滅する
+    public void roll(Player target) {
         System.out.println(this.name + " rolled!");
+        target.damaged(this.power);
     }
 }
